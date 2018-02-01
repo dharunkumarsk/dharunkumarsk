2ac76dcf51889c27cdfe6524c9106a912dd50f19
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-02-01 16:03:01 +0530

    Create alphabe.c

---
+++ alphabe.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    char c;
+    printf("enter the chracter:");
+    scanf("%c",&c);
+    if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+    {
+        printf("%c is an alphabet",c);
+    }
+    else
+    {
+      printf("%c is not an alphabet",c);  
+    }
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+About
