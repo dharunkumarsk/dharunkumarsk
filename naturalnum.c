3d257d7fe8234be8d2085b503436dd8fe0a5e835
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-02-06 16:43:45 +0530

    Create naturalnum.c

---
+++ naturalnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int b,i;
+    clrscr();
+    printf("Enter the N value:");
+    scanf("%d",&b);
+    i=(b*(b+1)/2);
+        printf("%d\n",i);
+    getch();
+}
