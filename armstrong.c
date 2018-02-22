73bccafdf2acf06699df380bf41de95591928e36
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-02-22 16:05:18 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int d ;
+printf("enter the n values  : ");
+scanf("%d",&d);
+for(d=0;d<1;d++)
+{
+if(d*2==0)
+{
+printf("this is armstrong number");
+}
+else
+{
+printf("not armstrong number");
+}
+}
+}
