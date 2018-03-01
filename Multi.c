2d1f7fe4c94f9c773ff233c5ad7cab4c7cac425d
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-03-01 16:00:48 +0530

    Create Multi.c

---
+++ Multi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int d;
+int n,a;
+printf("enter then number");
+scanf("%d",n);
+for(d=0;d<=n;d++)
+{
+a=d*5;
+printf("the muliple numbers are %d",a);
+}
+getch();
+}
