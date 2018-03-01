a6724fa65c5d1a817f2e07d4e6a32333890005f7
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-03-01 16:29:23 +0530

    Rename ams.c to amst.c

---
+++ amst.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num==0)
+{
+printf("armstrong number between two intrevels");
+}
+else
+{
+printf("not print");
+}
+}
