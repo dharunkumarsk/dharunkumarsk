99fd2988d0db77abbcce1b242a04d2ac3e265683
dharunkumarsk <35762110+dharunkumarsk@users.noreply.github.com>
2018-03-01 16:24:47 +0530

    Create armstrongs.c

---
+++ armstrongs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&number);
+if(number==0)
+{
+printf("armstrong number between two intrevels");
+}
+else
+{
+printf("not print");
+}
+}
